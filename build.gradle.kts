2e378636a0902138e40afed215b69e41e0a14b9c
jshopher <jshopher21@gmail.com>
2025-09-16 22:18:07 -0400

    First attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.jayway.jsonpath:json-path:2.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    implementation("net.minidev:json-smart:2.5.0")
 }
 
 tasks.test {
