f7b1a00858b4cf2ab6b0798cb703a10b49f6fa4d
jshopher <jshopher21@gmail.com>
2025-09-28 20:24:00 -0400

    Added plugin for GUI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,31 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
-group = "org.example"
+group = "edu.bsu.wiki"
 version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.slf4j:slf4j-nop:2.0.11")
     implementation("com.jayway.jsonpath:json-path:2.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     implementation("net.minidev:json-smart:2.5.0")
-    implementation("org.json:json:20240303")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+javafx {
+    version = "22"
+    modules("javafx.controls", "javafx.fxml")
+}
+
+application {
+    mainClass.set("edu.bsu.wiki.Main")
 }
